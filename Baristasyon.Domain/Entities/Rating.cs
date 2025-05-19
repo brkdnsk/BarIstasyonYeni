22d8cb7ea69e5be24a46e6b0099d99b2fca806a9
denizzylcn <116671202+meryemorc@users.noreply.github.com>
2025-05-19 19:49:34 +0300

    WebUI

---
+++ Baristasyon.Domain/Entities/Rating.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baristasyon.Domain.Entities
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CoffeeRecipeId { get; set; }
+        public int Score { get; set; } // 1–5
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
